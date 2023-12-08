ae1facf9c0b7bbadcf74cd54035ed1d1167b3356
hil679 <hil679922@gmail.com>
2023-12-08 19:31:38 +0900

    Feat(Table): 해당숫자 table 반환

--- src/main/java/domain/TableRepository.java
+++ src/main/java/domain/TableRepository.java
@@ -19,4 +19,11 @@ public class TableRepository {
     public static List<Table> tables() {
         return Collections.unmodifiableList(tables);
     }
+
+    public static Table findByNumber(int tableNumber) {
+        return tables.stream()
+                .filter(table -> table.isEqualTableNumber(tableNumber))
+                .findAny()
+                .get();
+    }
 }
