91bdc7fd09cfa4dff2cac6c1f408d897f676ac5e
hil679 <hil679922@gmail.com>
2023-12-08 14:36:32 +0900

    Feat(InputView): creaet skeleton code

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static int inputMain() {
+        return 0;
+    }
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
+    }
+
+    public static int inputMenu() {
+        return 0;
+    }
+
+
+    public static int inputMenuNum() {
+        return 0;
     }
 }
