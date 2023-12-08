ae33de6669857c6ddf13201f52b0b79116113d90
hil679 <hil679922@gmail.com>
2023-12-08 22:37:48 +0900

    Refactor(Order): 주문 내역 key 변경
    - key를 int에서 Menu로 변경

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Order {
-    Map<Integer, Integer> orders = new HashMap<>();
+    Map<Menu, Integer> orders = new HashMap<>();
 
     public void orderMenu(int menuUniqueNumber, int menuNum) {
-        orders.put(menuUniqueNumber, orders.getOrDefault(menuUniqueNumber, 0) + menuNum);
+        Menu menu = MenuRepository.findByNumber(menuUniqueNumber);
+        orders.put(menu, orders.getOrDefault(menu, 0) + menuNum);
     }
 
     public int getOrderMenuNum(int menuUniqueNumber) {
-        return orders.getOrDefault(menuUniqueNumber, 0);
+        Menu menu = MenuRepository.findByNumber(menuUniqueNumber);
+        return orders.getOrDefault(menu, 0);
     }
 }
