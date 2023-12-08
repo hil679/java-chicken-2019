d75fb65d43dac2b54cbe167160b91ed3b7bd52dc
hil679 <hil679922@gmail.com>
2023-12-08 19:32:14 +0900

    Feat(InputValidator): 메뉴 개수 제한 확인 기능 추가

--- src/main/java/view/validator/InputValidator.java
+++ src/main/java/view/validator/InputValidator.java
@@ -1,5 +1,6 @@
 package view.validator;
 
+import domain.Menu;
 import domain.MenuRepository;
 import domain.Table;
 import domain.TableRepository;
@@ -27,15 +28,28 @@ public class InputValidator {
                 .anyMatch(table -> table.isEqualTableNumber(tableNumber));
     }
 
-    public void checkMenu(int menuNumber) {
-        if(isNotMenu(menuNumber)) {
+    public void checkMenu(int menuUniqueNumber) {
+        if(isNotMenu(menuUniqueNumber)) {
             throw new IllegalArgumentException("[ERROR] 정확한 메뉴 번호를 입력해주세요.");
         }
     }
 
-    private boolean isNotMenu(int menuNumber) {
+    private boolean isNotMenu(int menuUniqueNumber) {
         return !MenuRepository.menus()
                 .stream()
-                .anyMatch(menu -> menu.isEqualMenuNumber(menuNumber));
+                .anyMatch(menu -> menu.isEqualMenuNumber(menuUniqueNumber));
+    }
+
+
+    public void checkMenuNumber(int menuNumber, int tableNumber, int menuUniqueNumber) {
+        if(isNotMenuNumber(menuNumber, tableNumber, menuUniqueNumber)) {
+            throw new IllegalArgumentException("[ERROR] 메뉴 가능 개수 범위를 벗어났습니다.");
+        }
+    }
+
+    private boolean isNotMenuNumber(int menuNumber, int tableNumber, int menuUniqueNumber) {
+        Table table = TableRepository.findByNumber(tableNumber);
+        int totalMunuNum = menuNumber + table.getOrderMenuNum(menuUniqueNumber);
+        return menuNumber < 1 || totalMunuNum > 99;
     }
 }
