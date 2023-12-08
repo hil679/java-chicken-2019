b5069fb0651c269d476c12e9cca20e95e5640259
hil679 <hil679922@gmail.com>
2023-12-08 14:56:03 +0900

    Feat(InputView): 메인 화면 기능 목록 입력 받기 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,25 @@
 package view;
 
+import view.validator.InputValidator;
+
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final InputValidator inputValidator = new InputValidator();
 
     public static int inputMain() {
-        return 0;
+        while(true) {
+            System.out.println("## 원하는 기능을 선택하세요.");
+
+            int mainFeatureNum = scanner.nextInt();
+            try {
+                inputValidator.checkMainFeatures(mainFeatureNum);
+                return mainFeatureNum;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 
     public static int inputTableNumber() {
