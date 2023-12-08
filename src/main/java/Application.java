c3a5f20667fddfdb41bcccba530b360e1cda076d
hil679 <hil679922@gmail.com>
2023-12-09 01:58:09 +0900

    Feat: pos 실행 코드 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,14 +8,34 @@ import view.OutputView;
 import java.util.List;
 
 public class Application {
-    // TODO 구현 진행
     public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
+        int mainFeature = getMainFeatures();
+        while(mainFeature != 3) {
+            final List<Table> tables = TableRepository.tables();
+            OutputView.printTables(tables);
 
-        final int tableNumber = InputView.inputTableNumber();
+            final int tableNumber = InputView.inputTableNumber();
+            Table table = TableRepository.findByNumber(tableNumber);
+            if(mainFeature == 1) {
+                final List<Menu> menus = MenuRepository.menus();
+                OutputView.printMenus(menus);
 
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
+                final int menuUniqueNumber = InputView.inputMenu();
+                final int menuNumber = InputView.inputMenuNum(tableNumber, menuUniqueNumber);
+                table.orderMenu(menuUniqueNumber, menuNumber);
+            } else{
+                int paymentMethod = InputView.inputPaymentMethod(tableNumber);
+                OutputView.printOrders(table);
+                OutputView.printTotalPayment(table, paymentMethod);
+                table.clearOrders();
+            }
+            mainFeature = getMainFeatures();
+        }
+
+    }
+
+    public static int getMainFeatures() {
+        OutputView.printMainFeatures();
+        return InputView.inputMain();
     }
 }
